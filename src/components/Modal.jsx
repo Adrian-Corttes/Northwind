f3f848b6e90d23d79b848d8ef133bfbbc42b1a4d
Adrian Cortes <ls-adrian@hotmail.com>
2022-11-15 12:02:34 -0500

    updated modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,6 +3,8 @@ import "./css/modal.css";
 const Modal = ({ id, Ciudad }) => {
   return (
     <div className="modal-w is-open">
+      <h3>Departamento</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda quae possimus consectetur voluptas, reiciendis rem nobis doloribus illo aspernatur molestiae rerum accusamus. Neque odit minima deserunt sapiente perferendis possimus ratione!</p>
       <table className="table">
         <thead className="table-dark">
           <tr>
